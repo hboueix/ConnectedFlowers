106c35e030ad0a2f024090c9f1689fff8fb103be
vivimouret29 <vivien.mouret@ynov.com>
2019-06-08 21:54:27 +0200

    last java

--- java/play_file.java
+++ java/play_file.java
@@ -1,56 +1,24 @@
-//Packages à importer afin d'utiliser les objets
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
 
-public class Main {
-   public static void main(String[] args) {
-      // Nous déclarons nos objets en dehors du bloc try/catch
-      FileInputStream fis = null;
-      FileOutputStream fos = null;
-      
+import java.io.*;
 
-      try {
-        // On instancie nos objets :
-        // fis va lire le fichier
-        fis = new FileInputStream(new File("java.txt"));
-
-        int i = 0;
-        // Tant que l'affectation dans la variable est possible, on boucle
-        // Lorsque la lecture du fichier est terminée l'affectation n'est
-        // plus possible !
-        // On sort donc de la boucle
-        while (i == 1) {
-            fis.read(fis);
-            i += i;
-
-
-        }
-        System.out.println("Lecture terminée !");
-
-      } catch (FileNotFoundException e) {
-         // Cette exception est levée si l'objet FileInputStream ne trouve
-         // aucun fichier
-         e.printStackTrace();
-      } finally {
-         // On ferme nos flux de données dans un bloc finally pour s'assurer
-         // que ces instructions seront exécutées dans tous les cas même si
-         // une exception est levée !
+public class ReadFile {
+      public static void main(String[] args){
+         String chaine="";
+         String fichier ="java.txt";
+         
+         //lecture du fichier texte 
          try {
-            if (fis != null)
-               fis.close();
-         } catch (IOException e) {
-            e.printStackTrace();
-         }
-
-         try {
-            if (fos != null)
-               fos.close();
-         } catch (IOException e) {
-            e.printStackTrace();
-         }
+            InputStream ips=new FileInputStream(fichier); 
+            InputStreamReader ipsr=new InputStreamReader(ips);
+            BufferedReader br=new BufferedReader(ipsr);
+            String ligne;
+            while ((ligne=br.readLine())!=null) {
+               System.out.println(ligne);
+               chaine+=ligne+"\n";
+            }
+         br.close(); 
+         } catch (Exception e) {
+      System.out.println(e.toString());
       }
    }
 }
--- java/windows.java
+++ java/windows.java
@@ -1,5 +1,8 @@
 import javax.swing.JFrame; 
-
+import java.sql.*;
+import java.awt.Graphics;
+import javax.swing.JPanel;
+import java.io.*;
  
 public class Fenetre {
   public static void main(String[] args) {
@@ -12,3 +15,35 @@ public class Fenetre {
     }
   }
  
+
+  public class File {
+    public static void main(String[] args){
+       String chaine="";
+       String fichier ="java.txt";
+       
+       //lecture du fichier texte 
+       try {
+          InputStream ips=new FileInputStream(fichier); 
+          InputStreamReader ipsr=new InputStreamReader(ips);
+          BufferedReader br=new BufferedReader(ipsr);
+          String ligne;
+          while ((ligne=br.readLine())!=null) {
+             System.out.println(ligne);
+             chaine+=ligne+"\n";
+          }
+       br.close(); 
+       } catch (Exception e) {
+    System.out.println(e.toString());
+    }
+ }
+}
+
+
+
+ 
+public class Panneau extends JPanel {
+  public void paintComponent(Graphics g){
+    String chaine = new File();
+    g.drawString(chaine, 10, 20);
+  }               
+}
