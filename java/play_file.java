f4a583526af0be5fb3b02af2442ae6961080799f
vivimouret29 <vivien.mouret@ynov.com>
2019-06-08 22:15:18 +0200

    last java

--- java/play_file.java
+++ java/play_file.java
@@ -1,23 +1,24 @@
-
 import java.io.*;
+
+
 
 public class ReadFile {
       public static void main(String[] args){
          String chaine="";
-         String fichier ="java.txt";
+         String fichier ="java.txt";                                 // choix du fichier à lire
          
          //lecture du fichier texte 
          try {
-            InputStream ips=new FileInputStream(fichier); 
-            InputStreamReader ipsr=new InputStreamReader(ips);
+            InputStream ips=new FileInputStream(fichier);            // classe les lignes du fichiers
+            InputStreamReader ipsr=new InputStreamReader(ips);       
             BufferedReader br=new BufferedReader(ipsr);
             String ligne;
-            while ((ligne=br.readLine())!=null) {
+            while ((ligne=br.readLine())!=null) {                    // fais une boucle ligne par ligne
                System.out.println(ligne);
                chaine+=ligne+"\n";
             }
-         br.close(); 
-         } catch (Exception e) {
+         br.close();            
+         } catch (Exception e) {                                     // ferme la fenetre s'il n'y a pas de fichier
       System.out.println(e.toString());
       }
    }
